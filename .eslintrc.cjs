378e3bdc03102241a2e7dac9d7594bc99297d44b
BaoNG <bao.nguyengia@petronas.com>
2023-06-15 20:57:50 +0700

    feat(): setup commitlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true, es2020: true, es2022: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
