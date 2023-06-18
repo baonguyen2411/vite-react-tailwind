d93482b1903af70ac8e1f727da1feaeaa58de612
BaoNG <bao.nguyengia@petronas.com>
2023-06-18 13:01:25 +0700

    feat(): add router folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { Collapse, Ripple, initTE } from 'tw-elements';
 
-import Routes from '@/components/Router/Routes';
+import Routes from '@/components/router/Routes';
 import AuthProvider from '@/components/layouts/AuthProvider';
 
 const App = () => {
